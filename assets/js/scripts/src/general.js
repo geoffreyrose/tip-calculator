35fe9e15eab0d73d2d0c9cabdc8e419f4427eafe
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2015-03-29 14:10:02 -0700

    js work

--- assets/js/scripts/src/general.js
+++ assets/js/scripts/src/general.js
@@ -1,3 +1,54 @@
+function totalamount() {
+	$('[name="bill-amount"]').priceFormat({
+		prefix: '$ ',
+	});
+	$('.total-value').priceFormat({
+		prefix: '$ ',
+	});
+	$('.your-total-value').priceFormat({
+		prefix: '$ ',
+	});
+
+	var split = $('.split .active').attr('data-split');
+	
+	var amount = $('[name="bill-amount"]').unmask();
+	amount = amount * 100;
+
+	var tip = $('[name="tip"]').val();
+	tip = tip / 100;
+	tip = (tip * amount);
+
+	var total = (amount +  tip) / 100;
+	total = total.toFixed(0);
+
+	var yourTotal = total / split;
+	yourTotal = yourTotal.toFixed(0);
+
+	$('.total-value').text(total);
+	$('.total-value').priceFormat({
+		prefix: '$ ',
+	});
+
+	$('.your-total-value').text(yourTotal);
+		$('.your-total-value').priceFormat({
+		prefix: '$ ',
+	});
+
+}
+
 $(function(){
-	$('[name="bill-amount"]').mask("9?99.99");
+	totalamount();
+
+	$('.split span').on('click', function(){
+		$('.split span').removeClass('active');
+		$(this).addClass('active');
+		totalamount();
+	});
+	$('[name="bill-amount"]').on('keyup', function(){
+		totalamount();	
+	});
+
+	$('[name="tip"]').on('change', function(){
+		totalamount();	
+	});
 });
